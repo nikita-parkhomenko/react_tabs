b1d19975dac092de83eac7e654f7084e0062b175
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-09-07 18:54:12 +0300

    Updates

---
+++ precache-manifest.30abb36a0bd560bdf2c91e28757de78c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9835373e0fddc78ca536c2d8e517c1e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1103a85cd0513888d1a4",
+    "url": "./static/css/main.dd7b35c2.chunk.css"
+  },
+  {
+    "revision": "c21235e66892e410b8ca",
+    "url": "./static/js/2.f6d830d2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.f6d830d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1103a85cd0513888d1a4",
+    "url": "./static/js/main.e7166e2a.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
